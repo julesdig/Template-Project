497ed858bbfe07bb13b1b8457c37dd173d17380d
jules <judigonnet@gmail.com>
2024-05-28 00:22:53 +0200

    test kernel

---
+++ backend/tests/KernelTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class KernelTest extends KernelTestCase
+{
+    public function testKernel()
+    {
+        self::bootKernel();
+        $kernel = self::$kernel;
+
+        $this->assertTrue($kernel instanceof Kernel);
+
+    }
+}
